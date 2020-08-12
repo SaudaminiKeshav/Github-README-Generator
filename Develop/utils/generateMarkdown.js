cf3223418dfb95f9ab7ffecead12f20eded87f3a
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-08-11 23:46:58 -0700

    Added readme content for title, installation, badge, license and description

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,31 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
+## Table of Contents 
 
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [License](#license)
+
+#${data.title}
+![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/${data.username}/${data.repo}?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${data.username}/${data.repo}?style=flat&logo=appveyor)
+  
+  Check out the badges hosted by [shields.io](https://shields.io/).
+  
+  ## Description 
+  
+  ${data.description}
+
+  ## Installation
+  
+  *Steps required to install project and how to get the development environment running:
+  
+  ${data.installation}
 `;
 }
 
