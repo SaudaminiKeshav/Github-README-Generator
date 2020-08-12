57808c2ac7e3466e943c4d6f0eedd5e7d162b1a7
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-08-11 22:33:39 -0700

    Added code logic to prompt asking user different questions

--- Develop/index.js
+++ Develop/index.js
@@ -2,15 +2,73 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
+function promptUser() {
+    return inquirer.prompt(questions);
+}
 const questions = [
-
+    {
+        type: 'input',
+        message: "What is your GitHub username?",
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repo?",
+        name: 'repo'
+    },
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Provide instructions and examples of your project in use for the Usage section.",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    }
 ];
 
 function writeToFile(fileName, data) {
 }
 
 function init() {
-
+    promptUser()
+        .then(function (answers) {
+           //Write to file
+        })
+        .then(function () {
+            console.log("Successfully wrote to index.html");
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
+
 
 init();
